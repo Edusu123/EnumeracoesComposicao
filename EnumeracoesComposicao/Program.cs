35aebed432ce195e960eb858370b58b45687190f
Eduardo Conde Pires <eduardopires@concilig.com.br>
2021-06-14 15:20:05 -0300

    Add project files.

---
+++ EnumeracoesComposicao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumeracoesComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
