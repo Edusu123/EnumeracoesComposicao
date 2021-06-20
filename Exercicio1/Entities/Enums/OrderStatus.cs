2ded385de9b8dc717913f39c540d0f9f5332972f
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-06-20 18:50:15 -0300

    Criação do enumerador OrderStatus

---
+++ Exercicio1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio1.Entities.Enums
+{
+    enum OrderStatus
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipping = 2,
+        Delivered = 3
+    }
+}
