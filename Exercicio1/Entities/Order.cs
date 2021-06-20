89ec0925c5064cced3793a906de806e61176bcbe
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-06-20 19:04:53 -0300

    Adição das entidades Order e OrderItem

---
+++ Exercicio1/Entities/Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Exercicio1.Entities.Enums;
+
+namespace Exercicio1.Entities
+{
+    class Order
+    {
+        #region Propriedades
+
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        #endregion
+
+        #region Construtores
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus orderStatus)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItems.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+
+            foreach(OrderItem item in OrderItems)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+        #endregion
+    }
+}
---
+++ Exercicio1/Entities/OrderItem.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio1.Entities
+{
+    class OrderItem
+    {
+        #region Propriedades
+
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; } = new Product();
+
+        #endregion
+
+        #region Construtores
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public double SubTotal()
+        {
+            Price = Quantity * Product.Price;
+            return Price;
+        }
+
+        #endregion
+    }
+}
