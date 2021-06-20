4e25346d35487875e902cc24f5508ed4c867d2f2
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-06-20 13:56:55 -0300

    Adição da classe Post

---
+++ Exemplo2/Entities/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exemplo2.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int liket)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Like = liket;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+    }
+}
